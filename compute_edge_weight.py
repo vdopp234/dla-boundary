e2f1476c955d06be96ed684292104d786d13cd3f
Vishnu Doppalapudi <vishnu234@berkeley.edu>
2019-12-02 09:24:01 -0800

    Modified edge weight script to directly take the inputs from cityscapes single instance dataset

--- compute_edge_weight.py
+++ compute_edge_weight.py
@@ -1,22 +1,27 @@
 from imageio import imread
 import numpy as np
-import os
-from boundary_utils import db_eval_boundary
+import os, torch
+from cityscapes_single_instance import CityscapesSingleInstanceDataset
 
 
 def main():
-    data_path = "../cityscapes_dataset/gtFine/train/"
-    cnt, weight = 0, 0
-    for folder in os.listdir(data_path):
-        curr_path = data_path + folder
-        for file in os.listdir(curr_path):
-            file_path = curr_path + '/' + file
-            if 'instanceId' in file:
-                cnt += 1
-                img = np.array(imread(file_path))
-                boundary_map = db_eval_boundary(img)
-                weight += np.sum(boundary_map)
-    print("Boundary Weight: ", weight/cnt)
+    data_dir = '../cityscapes_dataset'
+    num_workers = 8
+    train_loader = torch.utils.data.DataLoader(
+        CityscapesSingleInstanceDataset(data_dir, 'train', out_dir='edge_weight_outputs/'),
+        batch_size=1, shuffle=True, num_workers=num_workers,
+        pin_memory=True
+    )
+    cnt, bnd_cnt = 0, 0
+    for (input, target_seg, target_boundary, _) in train_loader:
+        bnd_cnt += np.sum(target_boundary)
+        cnt += np.product(target_boundary.shape)
+
+    f = open("edge_weight_outputs/edge_weight.txt")
+    f.write(str(cnt/bnd_cnt))
+    f.close()
+
+    print("Edge Weight: ", cnt/bnd_cnt)
 
 
 if __name__ == '__main__':
