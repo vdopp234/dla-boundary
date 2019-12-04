313c5d441fbadc9b1ea773c17617cc454eaf2d8f
Vishnu Doppalapudi <vishnu234@berkeley.edu>
2019-12-03 19:56:45 -0800

    More debugging

--- segment.py
+++ segment.py
@@ -228,8 +228,8 @@ def validate_segmentation(val_loader, model, criterion, epoch, writer, eval_scor
                 prediction = np.argmax(output.detach().cpu().numpy(), axis=1)
                 prob = torch.nn.functional.softmax(output.detach().cpu(), dim=1).numpy()
 
-                writer.add_image('validate/gt', np.expand_dims(target[0].cpu().numpy(), axis=0), step)
-                writer.add_image('validate/predicted', np.expand_dims(prediction[0], axis=0), step)
+                # writer.add_image('validate/gt', np.expand_dims(target[0].cpu().numpy(), axis=0), step)
+                # writer.add_image('validate/predicted', np.expand_dims(prediction[0], axis=0), step)
                 # writer.add_image('validate/prob', np.expand_dims(prob[0][1], axis=0), step)
                 print('Test: [{0}/{1}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
@@ -518,9 +518,9 @@ def train_seg(train_loader, model, criterion, optimizer, epoch, writer,
             # writer.add_image('train/gt', target[0].cpu().numpy(), step)
             # print("Target Shape: ", target.shape)
             # Expand Dims for compatibility with tensorboardX
-            writer.add_image('train/gt', np.expand_dims(target_seg[0].cpu().numpy(), axis=0), step)
-            writer.add_image('train/predicted', np.expand_dims(prediction[0], axis=0), step)
-            writer.add_image('train/prob', np.expand_dims(prob[0][0], axis=0), step)
+            # writer.add_image('train/gt', np.expand_dims(target_seg[0].cpu().numpy(), axis=0), step)
+            # writer.add_image('train/predicted', np.expand_dims(prediction[0], axis=0), step)
+            # writer.add_image('train/prob', np.expand_dims(prob[0][0], axis=0), step)
             print('Epoch: [{0}][{1}/{2}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
